8d40e273525b06ef7f670c2703b1b93c6445e37c
Damien <damien.marrou@gmail.com>
2021-10-28 20:43:12 +0200

    add views

--- lib/screens/splash_screen.dart
+++ lib/screens/splash_screen.dart
@@ -1,27 +1,25 @@
-
-
 import 'dart:async';
 
 import 'package:cupid_arrow/screens/welcome_screen.dart';
 import 'package:flutter/material.dart';
 
 class SplashScreen extends StatefulWidget {
-  static const String id ='splash-screen';
+  static const String id = 'splash-screen';
 
+  const SplashScreen({Key? key}) : super(key: key);
 
   @override
   State<SplashScreen> createState() => _SplashScreenState();
 }
 
 class _SplashScreenState extends State<SplashScreen> {
+  @override
   void initState() {
     Timer(
-        Duration(
+        const Duration(
           seconds: 5,
         ), () {
-
-          Navigator.pushReplacementNamed(context, WelcomeScreen.id);
-
+      Navigator.pushReplacementNamed(context, WelcomeScreen.id);
     });
     super.initState();
   }
@@ -30,20 +28,20 @@ class _SplashScreenState extends State<SplashScreen> {
   Widget build(BuildContext context) {
     return Scaffold(
       body: SafeArea(
-      child: Center(
-        child: Column(
-          mainAxisAlignment: MainAxisAlignment.center,
-          children:<Widget> [
-            Image.asset("images/logo_cupid1.png",
-              width: 250.0,
-              height: 250.0,
-            ),
-            Text('CUPID ARROW'),
-          ],
+        child: Center(
+          child: Column(
+            mainAxisAlignment: MainAxisAlignment.center,
+            children: <Widget>[
+              Image.asset(
+                "images/logo_cupid1.png",
+                width: 250.0,
+                height: 250.0,
+              ),
+              const Text('CUPID ARROW'),
+            ],
+          ),
         ),
       ),
-      ),
-
     );
   }
 }
